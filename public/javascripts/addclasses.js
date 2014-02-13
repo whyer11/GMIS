b40d2dd705bdbfbf0343b3b04533a4e9e736e9f3
王焕宇 <why490078184@gmail.com>
2014-02-13 17:00:34 +0800

    网页上又一点错误，正在排查。前端改的比较多。 想吐槽一下jade 这尼玛算什么前端模板引擎。真尼玛的头大！

--- public/javascripts/addclasses.js
+++ public/javascripts/addclasses.js
@@ -28,12 +28,10 @@ $(function () {
 
     }
 
-
     var allclasses;
+    var rootnode = [];
     var viewtreesettings = {
         view: {
-            addHoverDom: addHoverDom,
-            removeHoverDom: removeHoverDom,
             selectedMulti: false
         },
         edit: {
@@ -47,108 +45,11 @@ $(function () {
             }
         },
         callback: {
-            beforeDrag: beforeDrag,
-            beforeEditName: beforeEditName,
-            beforeRemove: beforeRemove,
-            beforeRename: beforeRename,
-            onRemove: onRemove,
-            onRename: onRename,
-            onNodeCreated: onNodeCreated,
             onClick: onClick
 
         }
 
     };
-    var log, className = "dark";
-
-    function beforeDrag(treeId, treeNodes) {
-        return false;
-    }
-
-    function beforeEditName(treeId, treeNode) {
-        className = (className === "dark" ? "" : "dark");
-        showLog("[ " + getTime() + " beforeEditName ]&nbsp;&nbsp;&nbsp;&nbsp; " + treeNode.name);
-        var zTree = $.fn.zTree.getZTreeObj("treeDemo");
-        zTree.selectNode(treeNode);
-        return confirm("进入节点 -- " + treeNode.name + " 的编辑状态吗？");
-    }
-
-    function beforeRemove(treeId, treeNode) {
-        className = (className === "dark" ? "" : "dark");
-        showLog("[ " + getTime() + " beforeRemove ]&nbsp;&nbsp;&nbsp;&nbsp; " + treeNode.name);
-        var zTree = $.fn.zTree.getZTreeObj("treeDemo");
-        zTree.selectNode(treeNode);
-        return confirm("确认删除 节点 -- " + treeNode.name + " 吗？");
-    }
-
-    function onRemove(e, treeId, treeNode) {
-        showLog("[ " + getTime() + " onRemove ]&nbsp;&nbsp;&nbsp;&nbsp; " + treeNode.name);
-    }
-
-    function beforeRename(treeId, treeNode, newName, isCancel) {
-        className = (className === "dark" ? "" : "dark");
-        showLog((isCancel ? "<span style='color:red'>" : "") + "[ " + getTime() + " beforeRename ]&nbsp;&nbsp;&nbsp;&nbsp; " + treeNode.name + (isCancel ? "</span>" : ""));
-        if (newName.length == 0) {
-            alert("节点名称不能为空.");
-            var zTree = $.fn.zTree.getZTreeObj("treeDemo");
-            setTimeout(function () {
-                zTree.editName(treeNode)
-            }, 10);
-            return false;
-        }
-        return true;
-    }
-
-    function onRename(e, treeId, treeNode, isCancel) {
-        showLog((isCancel ? "<span style='color:red'>" : "") + "[ " + getTime() + " onRename ]&nbsp;&nbsp;&nbsp;&nbsp; " + treeNode.name + (isCancel ? "</span>" : ""));
-    }
-
-    function showLog(str) {
-        if (!log) log = $("#log");
-        log.append("<li class='" + className + "'>" + str + "</li>");
-        if (log.children("li").length > 8) {
-            log.get(0).removeChild(log.children("li")[0]);
-        }
-    }
-
-    function getTime() {
-        var now = new Date(),
-            h = now.getHours(),
-            m = now.getMinutes(),
-            s = now.getSeconds(),
-            ms = now.getMilliseconds();
-        return (h + ":" + m + ":" + s + " " + ms);
-    }
-
-    var newCount = 1;
-
-    function addHoverDom(treeId, treeNode) {
-        var sObj = $("#" + treeNode.tId + "_span");
-        if (treeNode.editNameFlag || $("#addBtn_" + treeNode.tId).length > 0) return;
-        var addStr = "<span class='button add' id='addBtn_" + treeNode.tId
-            + "' title='add node' onfocus='this.blur();'></span>";
-        sObj.after(addStr);
-        var btn = $("#addBtn_" + treeNode.tId);
-        if (btn) btn.bind("click", function () {
-            var zTree = $.fn.zTree.getZTreeObj("treeDemo");
-            zTree.addNodes(treeNode, {id: (100 + newCount), pId: treeNode.id, name: "new node" + (newCount++)});
-            return false;
-        });
-    }
-
-    function removeHoverDom(treeId, treeNode) {
-        $("#addBtn_" + treeNode.tId).unbind().remove();
-    }
-
-    function selectAll() {
-        var zTree = $.fn.zTree.getZTreeObj("treeDemo");
-        zTree.setting.edit.editNameSelectAll = $("#selectAll").attr("checked");
-    }
-
-    //TODO
-    function onNodeCreated(treeId, treeNode) {
-
-    }
 
     function onClick(event, treeId, treeNode) {
         $.post('/render_current_node.json', {'id': treeNode.id}, function (data) {
@@ -158,8 +59,8 @@ $(function () {
 
     //TODO
     function renderNodeInfo(nodeInfos) {
-        $('#addclass_props').html('<h1>属性：</h1>');
-
+        $('#addclass_props').html('<h1>属性：</h1>' +
+            '<a class="btn btn-primary" id="addnewnode" data-target="#myModal" data-toggle="modal">新建子类型</a>');
         for (var i = 0; i < nodeInfos.length; i++) {
             $('#addclass_props').append('' +
                 '<a>属性名:</a>' +
@@ -169,7 +70,6 @@ $(function () {
 
     }
 
-    var rootnode = [];
     window.onload = function () {
         $.get('/display_class_name.json', {}, function (data) {
             allclasses = data;
