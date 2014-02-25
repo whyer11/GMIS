02599d81f095fcb9b33b8bf2563a392083669a89
王焕宇 <why490078184@gmail.com>
2014-02-25 16:59:56 +0800

    成功通过for循环添加对model的支持，正在添加测试模块

--- app/controllers/addclses_add_controller.js
+++ app/controllers/addclses_add_controller.js
@@ -1,6 +1,9 @@
 /**
  * Created by whyer on 14-2-9.
  */
+var orm = require('orm');
+var fs = require('fs');
+
 module.exports = function (req, res) {
     var gom_clses = req.models.gom_clses;
     var gom_props = req.models.gom_props;
@@ -8,3 +11,13 @@ module.exports = function (req, res) {
 
     res.end();
 };
+
+var newModel = function (modelName, modelContent) {
+    fs.open('./app/models/test.js', 'a', 0644, function (e, fd) {
+        if (e) throw e;
+        fs.write(fd, '', 0, 'utf8', function (e) {
+            if (e) throw e;
+            fs.closeSync(fd);
+        });
+    });
+}
--- app/models/index.js
+++ app/models/index.js
@@ -2,21 +2,29 @@
  * Created by whyer on 14-1-29.
  */
 var orm = require('orm'),
+    fs = require('fs'),
     settings = require('../../config/settings'),
     connection = null;
 
 function setup(db, cb) {
-    require('./gom_insts')(orm, db);
-    require('./gom_clses')(orm, db);
-    require('./gom_props')(orm, db);
-
+    var file = fs.readdirSync('./app/models');
+    addModels(file, db);
     return cb(null, db);
 }
-
+function addModels(file, db) {
+    for (var i = 0; i < file.length; i++) {
+        var modelName = file[i];
+        if (modelName == 'index.js') {
+            //遇到index跳过，不读取此文件
+        } else {
+            //将其他model读取
+            modelName = "./" + modelName.slice(0, -3);
+            require(modelName)(orm, db);
+        }
+    }
+}
 module.exports = function (cb) {
-
     orm.connect(settings.database, function (err, db) {
-
         db.settings.set('instance.returnAllErrors', true);
         setup(db, cb);
     });
