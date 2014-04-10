b3f610017c49108df59d4c932919eb4b1bbe9f19
王焕宇 <why490078184@gmail.com>
2014-04-10 08:50:35 +0800

    成功生成对象树

--- public/javascripts/app_display.js
+++ public/javascripts/app_display.js
@@ -1,3 +1,48 @@
 /**
  * Created by whyer on 14-3-23.
  */
+$(function(){
+    var curappid = $('#app').val();
+    var viewtreesettings = {
+        view: {
+            selectedMulti: false
+        },
+        edit: {
+            enable: false,
+            editNameSelectAll: false
+
+        },
+        data: {
+            simpleData: {
+                enable: true
+            }
+        },
+        callback: {
+            onClick:onClick
+        }
+    };
+    var nodes=[];
+    window.onload=function(){
+        $.post('/app_display.json',{appid:curappid},function(data){
+            console.log(data);
+            maketree(data);
+            console.log(nodes);
+            $.fn.zTree.init($('#treeDemo'),viewtreesettings,nodes);
+        })
+    }
+    function maketree(data){
+        for(var i = 0;i<data.length;i++){
+            var n = {
+                id:data[i].REF_ID,
+                pId:data[i].PARENT_REF_ID,
+                name:data[i].REF_NAME,
+                clsid:data[i].REF_CLS_ID
+            }
+            nodes.push(n);
+        }
+
+    }
+    function onClick(event,treeId,treeNode){
+
+    }
+})
