d909b53ff59d4caaeed63ad246d6daa1f80022d4
王焕宇 <why490078184@gmail.com>
2014-04-17 15:42:46 +0800

    对象浏览器,对象信息渲染基本实现

--- app/controllers/app_renderTreeNodeInfo_controller.js
+++ app/controllers/app_renderTreeNodeInfo_controller.js
@@ -5,45 +5,37 @@ var opt_db = require('../middleware/opt_db');
 var maps = require('../middleware/models_maps');
 var EventProxy = require('eventproxy');
 var ep = EventProxy();
-;module.exports = function(req,res){
+module.exports = function (req, res) {
     var currentNode = req.body;
-    checkParentsClass(req.body.clsid,req.body.instid);
-    function checkParentsClass (clsid,instid) {
+    checkParentsClass(req.body.clsid, req.body.instid);
+    function checkParentsClass(clsid, instid) {
         var nodeArray = {};
         var i = 0;
-        checkParentClass(clsid,clsid,i,instid);
-        function checkParentClass (classid,nextclassid,i,instid) {
-
-
-                req.models.gom_clses.get(nextclassid,function(err,clscols){
-                        //
-                        //console.log(clscols.CLS_TAB_NAME);
-                        //console.log(maps.modelsmaps(req,clscols.CLS_TAB_NAME));
-                        maps.modelsmaps(req,clscols.CLS_TAB_NAME).get(instid,function(err,data){
-                            //console.log(instid+'::'+ data +'::'+err);
-                            for (val in data){
-                                //console.log(val);
-                                if(val != 'GRANTOR_ID' && val != 'STATE_ID' && val != 'INSTACP_ID' && val != 'CLS_ID' && val != 'INST_NOTE' && val != 'INST_ID'){
-                                    nodeArray[val] = data[val];
+        checkParentClass(clsid, clsid, i, instid);
+        function checkParentClass(classid, nextclassid, i, instid) {
+            req.models.gom_clses.get(nextclassid, function (err, clscols) {
+                maps.modelsmaps(req, clscols.CLS_TAB_NAME).get(instid, function (err, data) {
+                    console.log(instid + '::' + data + '::' + err);
+                    req.models.gom_props.find({CLS_ID: nextclassid}, function (err, propcols) {
+                        for (var i = 0; i < propcols.length; i++) {
+                            for (val in data) {
+                                if (val == propcols[i].PROP_COL) {
+                                    nodeArray[propcols[i].PROP_NAME] = data[val];
                                 }
                             }
-                            i++;
-                            if(nextclassid == 0){
-                                res.json(nodeArray);
-                                res.end();
-                                //console.log(nodeArray);
-                            }else{
-                                nextclassid = clscols.PARENT_CLS_ID;
-                                checkParentClass(classid,nextclassid,i,instid);
-                            }
-
-                        });
-
-
-
-                })
-
+                        }
+                    });
+                    i++;
+                    if (nextclassid == 0) {
+                        res.json(nodeArray);
+                        res.end();
+                        //console.log(nodeArray);
+                    } else {
+                        nextclassid = clscols.PARENT_CLS_ID;
+                        checkParentClass(classid, nextclassid, i, instid);
+                    }
+                });
+            })
         }
     }
-
 }
