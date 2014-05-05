d79d5c58b965d3c4e61a3596a63ed34c7872eb93
王焕宇 <why490078184@gmail.com>
2014-05-05 11:18:24 +0800

    删除node_modules

--- app/controllers/app_renderTreeView_controller.js
+++ app/controllers/app_renderTreeView_controller.js
@@ -11,6 +11,7 @@ module.exports = function (req, res) {
     }
     var refs = [];
     var allnum = 0;
+    /*
     models.gom_appclses.find({APP_ID: appid}).each(function (appcol) {
         models.gom_insts.find({CLS_ID: appcol.CLS_ID}).each(function (instcol) {
             models.gom_refs.find({INST_ID: instcol.INST_ID}).count(function (err, num) {
@@ -31,6 +32,37 @@ module.exports = function (req, res) {
             });
         });
     });
-
+    */
+    console.log(appid);
+    models.gom_apps.get(appid,function(err,appcol){
+        console.log(appcol);
+        checkallrefs(appcol.REF_ID);
+        //console.log(refs);
+    })
+    function checkallrefs(pid){
+        models.gom_refs.find({PARENT_REF_ID:pid}).count(function(err,count){
+            if(count != 0){
+                models.gom_refs.find({PARENT_REF_ID:pid}).each(function(ref){
+                    if(typeof (ref) == 'undefined'){
+                        return 0;
+                    }else{
+                        console.log(ref.REF_ID);
+                        checkallrefs(ref.REF_ID);
+                        /*
+                        models.gom_insts.find({INST_ID:ref.INST_ID},function(err,inst){
+                            ref.REF_NAME = inst[0].INST_NAME;
+                            ref.REF_INST_ID = inst[0].INST_ID;
+                            ref.REF_CLS_ID = inst[0].CLS_ID;
+                            //console.log(ref);
+                            refs.push(ref);
+                            checkallrefs(ref.REF_ID)
+                        })
+                        */
+                    }
+                })
+            }
+        })
+    }
+    res.end();
 
 }
