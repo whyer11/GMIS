c76445bad13b0bc2ebf9b664d147be996c3582e0
王焕宇 <why490078184@gmail.com>
2014-05-06 10:26:31 +0800

    重构对象树生成控制器,之前的逻辑有问题.(善用EventProxy)

--- app/controllers/app_renderTreeView_controller.js
+++ app/controllers/app_renderTreeView_controller.js
@@ -1,6 +1,8 @@
 /**
  * Created by whyer on 14-4-3.
  */
+var EventProxy = require('eventproxy');
+var ep = new EventProxy();
 module.exports = function (req, res) {
     var appid = req.body.appid;
     var models = {
@@ -9,60 +11,59 @@ module.exports = function (req, res) {
         gom_refs: req.models.gom_refs,
         gom_apps: req.models.gom_apps
     }
-    var refs = [];
-    var allnum = 0;
-    /*
-    models.gom_appclses.find({APP_ID: appid}).each(function (appcol) {
-        models.gom_insts.find({CLS_ID: appcol.CLS_ID}).each(function (instcol) {
-            models.gom_refs.find({INST_ID: instcol.INST_ID}).count(function (err, num) {
-                allnum += num;
-                models.gom_refs.find({INST_ID: instcol.INST_ID}).each(function (refcol) {
-                    models.gom_insts.get(refcol.INST_ID, function (err, inst) {
-                        refcol.REF_NAME = inst.INST_NAME;
-                        refcol.REF_INST_ID = inst.INST_ID;
-                        refcol.REF_CLS_ID = inst.CLS_ID;
-                        refs.push(refcol);
-                        if (allnum == refs.length) {
-                            console.log(refs);
-                            res.json(refs);
-                            res.end();
-                        }
-                    });
-                });
-            });
-        });
+
+    models.gom_refs.find(['REF_ID', 'A'], function (err, refcols) {
+        ep.emit('returnallrefs', refcols);
     });
-    */
-    console.log(appid);
-    models.gom_apps.get(appid,function(err,appcol){
-        console.log(appcol);
-        checkallrefs(appcol.REF_ID);
+
+    models.gom_apps.get(appid, function (err, app) {
+        ep.emit('returnpid', app.REF_ID);
+    });
+
+    ep.all('returnallrefs', 'returnpid', function (refs, pid) {
+
+        var tree = [];
         //console.log(refs);
+        for (var i = 0; i < refs.length; i++) {
+            isChild(pid, refs[i], refs[i], refs, tree);
+        }
+        ep.emit('returntree', tree);
+    });
+
+    ep.all('returntree', function (tree) {
+
+        addProperties(0, tree);
+    });
+
+    ep.all('propertyall', function (tree) {
+        //console.log(tree[0].CLS_ID)
+        res.json(tree);
+        res.end();
     })
-    function checkallrefs(pid){
-        models.gom_refs.find({PARENT_REF_ID:pid}).count(function(err,count){
-            if(count != 0){
-                models.gom_refs.find({PARENT_REF_ID:pid}).each(function(ref){
-                    if(typeof (ref) == 'undefined'){
-                        return 0;
-                    }else{
-                        console.log(ref.REF_ID);
-                        checkallrefs(ref.REF_ID);
-                        /*
-                        models.gom_insts.find({INST_ID:ref.INST_ID},function(err,inst){
-                            ref.REF_NAME = inst[0].INST_NAME;
-                            ref.REF_INST_ID = inst[0].INST_ID;
-                            ref.REF_CLS_ID = inst[0].CLS_ID;
-                            //console.log(ref);
-                            refs.push(ref);
-                            checkallrefs(ref.REF_ID)
-                        })
-                         *//////
-                    }
-                })
+    function isChild(pid, c_obj, next, ref_array, tree_array) {
+        if (pid == next.REF_ID) {
+            tree_array.push(c_obj);
+        } else {
+            for (var i = 0; i < ref_array.length; i++) {
+                if (ref_array[i].REF_ID == next.PARENT_REF_ID) {
+                    isChild(pid, c_obj, ref_array[i], ref_array, tree_array);
+                }
             }
-        })
+        }
     }
-    res.end();
+
+    function addProperties(i, tree) {
+        if (i < tree.length) {
+            models.gom_insts.get(tree[i].INST_ID, function (err, inst) {
+                tree[i].REF_NAME = inst.INST_NAME;
+                tree[i].REF_CLS_ID = inst.CLS_ID;
+                tree[i].REF_INST_ID = inst.INST_ID;
+                i++;
+                addProperties(i, tree);
+            })
+        } else {
+            ep.emit('propertyall', tree);
+        }
+    }
 
 }
